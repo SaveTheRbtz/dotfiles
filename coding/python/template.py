87cabae4bf5fd80083a758e95d64879029cc40dc
Alexey Ivanov <SaveTheRbtz@GMail.com>
2012-04-15 22:26:01 +0400

    [python] Fixed misstype. Vim encoding. OptionParser.

--- coding/python/template.py
+++ coding/python/template.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 :
 
 def main():
     pass
 
-if __name__ == '__main__'
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    #parser.add_option("-f", type="string", dest="file", default="")
+    (options, args) = parser.parse_args()
+
     main()
